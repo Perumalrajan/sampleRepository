efdbe250a50b2f63c4a905465a5e52785abeb56c
Perumalrajan <perumalrajan100@gmail.com>
2025-08-01 11:43:29 +0530

    Version1 of Login file

---
+++ Login.jsx
@@ -0,0 +1,44 @@
+// Login.jsx - Simple functional version
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login submitted:', form);
+  };
+
+  return (
+    <div style={{ maxWidth: '300px', margin: 'auto' }}>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
